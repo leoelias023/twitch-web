c10ce0b922a8caae3615a6769ad6855a27406c1c
Leonardo Elias <leoelias02@hotmail.com>
2020-04-05 23:28:48 -0300

    just evaluate the code

--- src/components/AsideLives/index.js
+++ src/components/AsideLives/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Users from '~/data/users';
+
 import ItemChannel from '~/components/ItemChannel/index';
 
 import { AsideBox, IconAside, ListChannels, LinkAside } from './styles';
@@ -7,55 +9,25 @@ import { AsideBox, IconAside, ListChannels, LinkAside } from './styles';
 import { ReactComponent as MyFriendsLogo } from '~/assets/my_friends_icon.svg';
 
 export default function AsideLives() {
+  const Element = Users.map((user) => {
+    return (
+      <ItemChannel
+        key={user.name}
+        name={user.name}
+        category={user.category}
+        transmission={user.transmission}
+        isOn
+        icon={user.iconUrl}
+      />
+    );
+  });
+
   return (
     <AsideBox>
       <IconAside to="/">
         <i className="far fa-heart" />
       </IconAside>
-      <ListChannels>
-        <ItemChannel
-          name="collabcode"
-          category="Ciência e tecnologia"
-          transmission="Trabalhando em um freela wordpress"
-          isOn
-          icon="https://static-cdn.jtvnw.net/jtv_user_pictures/4f3bdf9f-6cc2-4148-8ffc-502707680ca5-profile_image-70x70.png"
-        />
-        <ItemChannel
-          name="edu"
-          category="Ciência e tecnologia"
-          transmission="Trabalhando em um freela wordpress"
-          icon="https://static-cdn.jtvnw.net/jtv_user_pictures/0ebc748f-8c79-4831-b105-c10b9ffa957e-profile_image-70x70.png"
-          isOn
-        />
-        <ItemChannel
-          isOn={false}
-          name="livinginweelchair"
-          category="Ciência e tecnologia"
-          transmission="Trabalhando em um freela wordpress"
-          icon="https://static-cdn.jtvnw.net/jtv_user_pictures/5d60c341-feb0-431d-bff0-e9c8145db5f8-profile_image-70x70.png"
-        />
-        <ItemChannel
-          name="test4"
-          category="Ciência e tecnologia"
-          transmission="Trabalhando em um freela wordpress"
-          icon="https://static-cdn.jtvnw.net/jtv_user_pictures/0ebc748f-8c79-4831-b105-c10b9ffa957e-profile_image-70x70.png"
-          isOn
-        />
-        <ItemChannel
-          name="test5"
-          category="Ciência e tecnologia"
-          transmission="Trabalhando em um freela wordpress"
-          icon="https://static-cdn.jtvnw.net/jtv_user_pictures/0ebc748f-8c79-4831-b105-c10b9ffa957e-profile_image-70x70.png"
-          isOn
-        />
-        <ItemChannel
-          name="test6"
-          category="Ciência e tecnologia"
-          transmission="Trabalhando em um freela wordpress"
-          icon="https://static-cdn.jtvnw.net/jtv_user_pictures/0ebc748f-8c79-4831-b105-c10b9ffa957e-profile_image-70x70.png"
-          isOn
-        />
-      </ListChannels>
+      <ListChannels>{Element}</ListChannels>
       <LinkAside to="/">
         <MyFriendsLogo />
       </LinkAside>
---
+++ src/data/users.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    name: 'EduardoRFS',
+    category: 'Ciência e tecnologia',
+    transmission: 'busque.dev | React + TS',
+    iconUrl:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/0ebc748f-8c79-4831-b105-c10b9ffa957e-profile_image-50x50.png',
+  },
+  {
+    name: 'DidiSoftwares',
+    category: 'Ciência e tecnologia',
+    transmission: 'pai no céu tem puppeteer ? e morreu.',
+    iconUrl:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/4ea91437-cfed-4c25-a6a0-759d34b5ec29-profile_image-50x50.jpg',
+  },
+  {
+    name: 'andrelmlins',
+    category: 'Ciência e tecnologia',
+    transmission:
+      'Desenvolvendo a aplicação YourFreeTime em React de uma forma bem diferente.',
+    iconUrl:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/03bb9672-bf7e-4aef-8774-5b9123859df7-profile_image-50x50.png',
+  },
+  {
+    name: 'collabcode',
+    category: 'Ciência e tecnologia',
+    transmission: 'Fazendo um freelancer em #PHP',
+    iconUrl:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/4f3bdf9f-6cc2-4148-8ffc-502707680ca5-profile_image-70x70.png',
+  },
+];
